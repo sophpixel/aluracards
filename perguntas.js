9e6bac6df1d4dc8fb51363ce7499a5a44b4635bc
sophpixel <00001103309407sp@al.educacao.sp.gov.br>
2024-11-04 11:37:41 -0300

    perguntas cards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sistema solar',
+    'Quantos planetas existem?',
+    'oito planetas'
+)
+
+criaCartao(
+    'Sistema Solar',
+    'Qual o maior planeta?',
+    'Jupter'
+)
+
+criaCartao(
+    'idiomas',
+    'Qual idioma mais falado?',
+    'Ingles'
+)
+
+criaCartao(
+    'Mundo Animal',
+    'O que os pandas comem?',
+    'Bambu'
+)
